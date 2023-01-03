325ff620efabd166b654a73c8027578daa109174
LeeGongnan <elsif@foxmail.com>
2023-01-04 00:24:50 +0800

    增加抽选结果统计

--- Send_Img_Group/script/reply_send_img_group.lua
+++ Send_Img_Group/script/reply_send_img_group.lua
@@ -3,39 +3,32 @@ notice_head = ".send qq"
 local send_flag = string.match(msg.fromMsg,"^[%s]*(.-)[%s]*$",#"&余量推送"+1)
 
 
--- local image = string.sub(text, string.find(text,"image")-4)
-
-
--- qqnums = {"204978819", "746516427"}
--- for i, qqnum in pairs(qqnums) do
---     sendMsg(image, qqnum, nil)
---     sleepTime(5000)
--- end
-
-test_group = {"667546784"}
-
--- remaining_image = string.sub(text, string.find(text,"image")-4)
+if( send_flag == "0" )
+then
+    cur_msg = "余量更新停止"
+    sendMsg(cur_msg, nil, "429146733")
+else
+    cur_msg = "余量自动更新开始"
+    sendMsg(cur_msg, nil, "429146733")
+    remaining_image = string.sub(send_flag, string.find(send_flag,"image")-4)
+    send_flag = "1"
+end
 
 local file1=io.output("local_flag.txt")
 io.write(send_flag)
 io.flush()
 io.close()
 
-if( send_flag == "1" )
-then
-    cur_msg = "余量自动更新开始"
-    sendMsg(cur_msg, nil, "429146733")
-else
-    cur_msg = "余量更新停止"
-    sendMsg(cur_msg, nil, "429146733")
-end
+test_group = {"667546784"}
+market_group = {"204978819"}
 
 while(send_flag == "1")
 do
-    for i, qqnum in pairs(test_group) do
-        sendMsg("余量2", nil, "429146733")
-        local time_random = math.random(9000)
-        sleepTime(5000+time_random)
+    for i, group_num in pairs(market_group) do
+
+        sendMsg(remaining_image, group_num, nil)
+        local time_random = math.random(1060000)
+        sleepTime(36000000+time_random)
     end
     local file1=io.input("local_flag.txt")
     send_flag = io.read("*a")
